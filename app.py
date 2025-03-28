8ccb475aec3d88cfab3bb5518eb5d0a92594120e
Ray-Chen13 <rayc35@nycstudents.net>
2025-03-28 09:22:33 -0400

    Ummm so like this just looks like letters and symbols to me

--- app.py
+++ app.py
@@ -1,14 +1,39 @@
 import json
-x = input("Teacher or Student")
 
-if x == "Teacher":
-    class Teacher:
-        def __init__(self, word, answer):
-            self.word= word
-            self.answer= answer
+def save_flashcards(flashcards_data):
+    with open("FlashCards.json", "w") as file:
+        json.dump(flashcards_data, file, indent=4)  
 
-        def flashcards(self):
-            return f"{self.word}"
+def load_flashcards(flashcards_data):
+    try:
+        with open("FlashCards.json", "r") as file:
+            return json.file(file)
+    except FileNotFoundError:
+        return []
+
+x = input("Would you like to enter Teacher or Student: ")
+
+while x != "Exit":
+    if x == "Teacher":
+        class Teacher:
+            def __init__(self, word, answer):
+                self.word= word
+                self.answer= answer
+
+            def flashcards(self):
+                return f"{self.word}: {self.answer}"
     
-words= Teacher(input("Tell me a word: ")), input("Give me an answer: ")
-print(words.flashcards())
+        words = Teacher(input("Tell me a word: "), input("Give me an answer: "))
+        print(words.flashcards())
+
+        flashcards_data = load_flashcards()
+
+        word = input("Tell me a word: ")
+        answer = input("Give me an answer: ")
+
+        new_flashcard = Teacher(word,answer)
+
+        flashcards_data.append({"word": word, "answer": answer})
+
+        save_flashcards(flashcards_data)
+
